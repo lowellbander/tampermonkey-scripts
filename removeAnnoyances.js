fb4471a942cefb4dd2c3a2a48f8e98dd1b343c71
Lowell Bander <lowellbander@gmail.com>
2018-01-09 09:18:41 -0800

    Create removeAnnoyances.js

---
+++ removeAnnoyances.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Remove Annoyances
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Remove Intern Notification Globe & Chat
+// @author       Lowell Bander
+// @match        *.facebook.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function removeAnnoyances() {
+      [
+          "a[href$='/intern/inbox/']", // notification globe in intern
+          "a[aria-label='Chat']", // chat icon in intern
+          "div[id='pagelet_bluebar']", // workplace bar in workchat
+      ]
+      .map(selector => document.querySelector(selector))
+      .forEach(annoyance => annoyance && annoyance.remove());
+    }
+    document.addEventListener("DOMNodeInserted", removeAnnoyances);
+})();
